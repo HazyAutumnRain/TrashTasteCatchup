c977725a32b8153b3076adac2467bd6cbbb0d599
Hazyautumnrain <renablehots@gmail.com>
2022-06-09 08:01:30 +0400

    vidoes page complete

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function modeSwitch() {
-    document.body.classList.toggle("light");
-    if (document.body.classList.contains("light")) {
-        document.getElementById("modeicon").src = "icons/moon.svg";
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        document.getElementById("modeicon").src = "icons/sun.svg";
         document.getElementById("modeicon").classList.toggle("buttondark");
     } else {
-        document.getElementById("modeicon").src = "icons/sun.svg";
+        document.getElementById("modeicon").src = "icons/moon.svg";
         document.getElementById("modeicon").classList.toggle("buttondark");
     }
 }
