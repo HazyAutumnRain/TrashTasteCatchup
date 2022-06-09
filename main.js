8ab6a54c602a130e0236578edf9395f364aa03de
Hazyautumnrain <renablehots@gmail.com>
2022-06-09 12:32:04 +0400

    beta commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const swup = new Swup();
+
 function modeSwitch() {
     document.body.classList.toggle("dark");
     if (document.body.classList.contains("dark")) {
@@ -8,6 +10,13 @@ function modeSwitch() {
         document.getElementById("modeicon").classList.toggle("buttondark");
     }
 }
+
+swup.on('contentReplaced', function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+});
 
 var parent = ['sites.google.com', 'www.gstatic.com', 'plus.destructomc.com', window.location.host];
 
